6bf511230a85fb10d387268851fa7c1ca2f64c34
kjpar0317 <kjpar_kr@yahoo.co.kr>
2022-07-11 10:30:39 +0900

    daisyui 적용

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,15 +1,10 @@
 // See the Tailwind configuration guide for advanced usage
 // https://tailwindcss.com/docs/configuration
 module.exports = {
-  content: [
-    './js/**/*.js',
-    '../lib/*_web.ex',
-    '../lib/*_web/**/*.*ex'
-  ],
-  theme: {
+  content: ["./js/**/*.js", "../lib/*_web.ex", "../lib/*_web/**/*.*ex"],
+  theme: {},
+  variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/forms')
-  ]
-}
+  plugins: [require("daisyui")],
+};
